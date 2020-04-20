35ca78c9005684f9d80ccd580d60fe28c9ad5b47
Javier Quinteros <javier@nora.cl>
2020-04-19 23:08:46 -0400

    Added Json and MenuProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
+import 'package:componentes/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componentes',
-      home: HomePageTemp(),
+      home: HomePage(),
     );
   }
 }
